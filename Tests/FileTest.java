1affe6d1bcbe31135ba82cdbad4a9484ea0ddd0a
wietzeschr <schryvers.wietze@gmail.com>
2024-03-15 10:15:00 +0100

    Tests Filebuffer

--- Tests/FileTest.java
+++ Tests/FileTest.java
@@ -1,5 +1,10 @@
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class FileTest {
+    @Test
+    void create() {
 
+    }
 }
--- Tests/StackedLayoutTest.java
+++ Tests/StackedLayoutTest.java
@@ -1,5 +1,11 @@
+import org.junit.jupiter.api.Test;
+
+import java.awt.*;
+import java.io.FileNotFoundException;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class StackedLayoutTest {
+
 
 }
