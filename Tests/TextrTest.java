5958d7a80e703b80ecb09b592d28bda01997b348
wietzeschr <schryvers.wietze@gmail.com>
2024-03-15 15:19:26 +0100

    useCaseTests2

--- Tests/TextrTest.java
+++ Tests/TextrTest.java
@@ -31,13 +31,46 @@ public class TextrTest {
         // Tests Add New Line Break
         test1.addNewLineBreak();
         assertEquals(fbv1.getContent().length, 4);
+        assertEquals(fbv1.getContent()[0], "rij1!");
         assertEquals(fbv1.getContent()[1], "");
         // Tests Delete Char
+        test1.deleteChar();
+        assertEquals(fbv1.getContent().length, 3);
+        assertEquals(fbv1.getContent()[0], "rij1!");
+        assertEquals(fbv1.getContent()[1], "rij2");
+        assertEquals(fbv1.getContent()[2], "rij3");
+        test1.deleteChar();
+        assertEquals(fbv1.getContent().length, 3);
+        assertEquals(fbv1.getContent()[0], "rij1");
+        assertEquals(fbv1.getContent()[1], "rij2");
+        assertEquals(fbv1.getContent()[2], "rij3");
     }
 
     @Test
     void testChangeFocus() {
-
+        // Initialisatie Textr
+        File f1 = new File("test1", new FileBuffer(new String[] {"rij1", "rij2","rij3"}));
+        FileBufferView fbv1 = new FileBufferView(1,1,new Point(1,1),f1 );
+        File f2 = new File("test2", new FileBuffer(new String[] {"t", "te", "tes", "test"}));
+        FileBufferView fbv2 = new FileBufferView(1, 1, new Point(1,1), f2);
+        SideBySideLayout sbsl1 = new SideBySideLayout(1, 1, new Point(1,1), new Layout[] {fbv1, fbv2});
+        File f3 = new File("test3", new FileBuffer(new String[] {"h", "ha","hal", "hall", "hallo"}));
+        FileBufferView fbv3 = new FileBufferView(1,1,new Point(1,1),f3 );
+        StackedLayout sl1 = new StackedLayout(1, 1, new Point(1,1), new Layout[] {sbsl1, fbv3});
+        Textr test1 = new Textr("\n", sl1);
+        test1.initViewPositions();
+        test1.updateSize(20, 40);
+        // Tests
+        test1.changeFocusNext();
+        assertEquals(test1.getFocus(), 2);
+        test1.changeFocusNext();
+        assertEquals(test1.getFocus(), 3);
+        test1.changeFocusNext();
+        assertEquals(test1.getFocus(), 1);
+        test1.changeFocusPrevious();
+        assertEquals(test1.getFocus(), 3);
+        test1.changeFocusPrevious();
+        assertEquals(test1.getFocus(), 2);
     }
 
     @Test
